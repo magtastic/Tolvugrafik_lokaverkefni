f43e39281c520b1f5e6ad6d0ef8f879bc42d7785
magtastic <magnusol93@gmail.com>
2016-03-31 20:15:07 +0000

    Froskur!
    Kominn með forsk yo!
    
    Stækkaði þetta allt svo þetta skalar frá y{ 55 - (-55) } og x{ 55 -
    (-55) }.  Spá í að hafa þetta 11x11 og hvert hnit er þá 10x10. eða
    skiluruh?

--- Frogger.js
+++ Frogger.js
@@ -17,6 +17,7 @@ var GRAY = vec4(0.4, 0.4, 0.4, 1.0);
 
 var riverBuffer;
 var roadBuffer;
+var frogBuffer;
 
 var vPosition;
 var colorLoc;
@@ -24,14 +25,35 @@ var mvLoc;
 var pLoc;
 var proj;
 
+var frogVertices = [
+    // front side:
+    vec3( -5,  45,  10 ), vec3( -5, 45,  0 ), vec3(  5, 45,  0 ),
+    vec3(  5, 45,  0 ), vec3(  5,  45,  10 ), vec3( -5,  45,  10 ),
+    // right side:
+    vec3(  5,  45, 10 ), vec3(  5, 45,  0 ), vec3(  5, 55, 0 ),
+    vec3(  5, 55, 10 ), vec3(  5,  55, 10 ), vec3(  5,  45,  10 ),
+    // bottom side:
+    vec3(  5, 45,  0 ), vec3( -5, 45,  0 ), vec3( -5, 55, 0 ),
+    vec3( -5, 55, 0 ), vec3(  5, 55, 0 ), vec3(  5, 45,  0 ),
+    // top side:
+    vec3(  5,  55, 10 ), vec3( -5,  55, 10 ), vec3( -5,  45,  10 ),
+    vec3( -5,  45,  10 ), vec3(  5,  45,  10 ), vec3(  5,  55, 10 ),
+    // back side:
+    vec3( -5, 55, 0 ), vec3( -5,  55, 10 ), vec3(  5,  55, 10 ),
+    vec3(  5,  55, 10 ), vec3(  5, 55, 0 ), vec3( -5, 55, 0 ),
+    // left side:
+    vec3( -5,  55, 10 ), vec3( -5, 55, 0 ), vec3( -5, 45,  0 ),
+    vec3( -5, 45,  0 ), vec3( -5,  45,  10 ), vec3( -5,  55, 10 )
+];
+
 var riverVertices = [
-  vec3( 1, 0, 0.0 ), vec3( 1, -1, 0.0 ),
-  vec3( -1, 0, 0.0 ), vec3( -1, -1, 0.0 )
+    vec3( 55, -15, 0.0 ), vec3( 55, -55, 0.0 ),
+    vec3( -55, -15, 0.0 ), vec3( -55, -55, 0.0 )
 ];
 
 var roadVertices = [
-  vec3( 1, 1, 0.0 ), vec3( 1, 0, 0.0 ),
-  vec3( -1, 1, 0.0 ), vec3( -1, 0, 0.0 )
+    vec3( 55, 45, 0.0 ), vec3( 55, -5, 0.0 ),
+    vec3( -55, 45, 0.0 ), vec3( -55, -5, 0.0 )
 ];
 
 window.onload = function init()
@@ -56,6 +78,9 @@ window.onload = function init()
     gl.bindBuffer( gl.ARRAY_BUFFER, riverBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(riverVertices), gl.STATIC_DRAW );
 
+    frogBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, frogBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(frogVertices), gl.STATIC_DRAW );
 
     roadBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, roadBuffer );
@@ -99,17 +124,39 @@ function drawRiver(mv) {
 
 }
 
+function drawFrog(mv) {
+
+    gl.uniform4fv( colorLoc, RED );
+    gl.bindBuffer( gl.ARRAY_BUFFER, frogBuffer );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, 36 );
+
+}
+
+function initMv() {
+
+  var mv = mat4();
+  mv = lookAt( vec3(0.0, 3.0, 2.0),
+               vec3(0.0, 0.0, 0.0),
+               vec3(0.0, 0.0, 1.0) );
+
+  mv = mult( mv, scalem( 0.02 , 0.02 , 0.02 ) );
+
+  return mv;
+
+}
+
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var mv = mat4();
-    mv = lookAt( vec3(0.0, 3.0, 2.0),
-                 vec3(0.0, 0.0, 0.0),
-                 vec3(0.0, 0.0, 1.0) );
+    var mv = initMv();
 
     drawRoad(mv);
     drawRiver(mv);
+    drawFrog(mv);
 
     requestAnimFrame( render );
 }
